219704368137fc7c109355f16ef8af17da9f54bb
Vinay Hasyagar <vinaya.hasyagara@happiestminds.com>
2021-08-19 10:41:46 +0530

    August19commit

---
+++ gateway_debug.c
@@ -0,0 +1,32 @@
+15:02:29 PUBLISH | IS_DUP: false | RETAIN: 0 | QOS: DELIVER_AT_LEAST_ONCE | 
+TOPIC_NAME: devices/MT-Cond-21315798/messages/events/topic=stockpiletelemetry | PACKET_ID: 1979 | PAYLOAD_LEN: 434\r\n
+Error: Time:Mon Jun  7 15:13:47 2021 File:/home/vinay/Advantech/debugprints/Firmware-Gateway_multi/azure-iot-sdk-c/c-utility/adapters/socketio_berkeley.c 
+Func:socketio_dowork Line:1064 Socketio_Failure: Receiving data from endpoint: errno=110.\r\n
+iothub_connection_status, connected:0, stop_running:1 \n-> 
+15:13:49 CONNECT | VER: 4 | KEEPALIVE: 240 | FLAGS: 128 | 
+USERNAME: budig-bb-ihub-01-p.azure-devices.net/MT-Cond-21315798/?api-version=2020-09-30&DeviceClientType=iothubclient%2f1.3.9%20(native%3b%20Linux%3b%20armv5tejl) | 
+CLEAN: 0\r\n<- 15:13:49 CONNACK | SESSION_PRESENT: true | RETURN_CODE: 0x0\r\n
+iothub_connection_status, connected:1, stop_running:1 \n-> 
+15:13:49 SUBSCRIBE | PACKET_ID: 1980 | TOPIC_NAME: devices/MT-Cond-21315798/messages/devicebound/# | QOS: 1\r\n<- 15:13:49 SUBACK | PACKET_ID: 1980 | RETURN_CODE: 1
+
+
+9:04:41 PINGREQ\r\n<- 09:04:42 PINGRESP\r\n-> 
+09:08:41 PINGREQ\r\n<- 09:08:42 PINGRESP\r\n-> 
+09:12:41 PINGREQ\r\n<- 09:12:42 PINGRESP\r\n-> 
+09:16:41 PINGREQ\r\n<- 09:16:42 PINGRESP\r\n-> 
+09:20:42 PINGREQ\r\n<- 09:20:42 PINGRESP\r\n-> 
+09:24:42 PINGREQ\r\n<- 09:24:42 PINGRESP\r\n-> 
+09:28:42 PINGREQ\r\n<- 09:28:42 PINGRESP\r\n-> 
+09:32:42 PINGREQ\r\n<- 09:32:42 PINGRESP\r\n-> 
+09:36:42 PINGREQ\r\n<- 09:36:42 PINGRESP\r\n-> 
+09:40:42 PINGREQ\r\n<- 09:40:42 PINGRESP\r\n-> 
+09:44:42 PINGREQ\r\n<- 09:44:42 PINGRESP\
+
+
+Error: Time:Tue Jun  8 09:13:32 2021 File:/home/vinay/Advantech/debugprints/Firmware-Gateway_multi/azure-iot-sdk-c/c-utility/adapters/socketio_berkeley.c 
+Func:socketio_dowork Line:1064 Socketio_Failure: Receiving data from endpoint: errno=110.\r\n
+iothub_connection_status, connected:0, stop_running:1 \n-> 
+09:13:34 CONNECT | VER: 4 | KEEPALIVE: 240 | FLAGS: 128 | USERNAME: budig-bb-ihub-01-p.azure-devices.net/MT-Cond-21315798/?api-version=2020-09-30&DeviceClientType=iothubclient%2f1.3.9%20(native%3b%20Linux%3b%20armv5tejl) | CLEAN: 0\r\n<- 
+09:13:34 CONNACK | SESSION_PRESENT: true | RETURN_CODE: 0x0\r\niothub_connection_status, connected:1, stop_running:1 \n-> 
+09:13:34 SUBSCRIBE | PACKET_ID: 2066 | TOPIC_NAME: devices/MT-Cond-21315798/messages/devicebound/# | QOS: 1\r\n<- 
+09:13:34 SUBACK | PACKET_ID: 2066 | RETURN_CODE: 1
